3293be72d13052c385f5eccb204806db77b45832
Casey <caseyjtite@gmail.com>
2022-01-19 15:11:57 -0800

    enable exit

--- play.js
+++ play.js
@@ -1,7 +1,25 @@
 const net = require("net");
+const { stdin } = require("process");
 const { connect } = require("./client");
 
-// establishes a connection with the game server
+const handleUserInput = function () {
+  //terminates game
+  stdin.on("data", (key) => {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  });
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
 console.log("Connecting ...");
 connect();
+setupInput();
