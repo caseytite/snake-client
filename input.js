a274b2ed68d77f12ab1df617a1538f26eb7578ca
Casey <caseyjtite@gmail.com>
2022-01-19 15:20:24 -0800

    refactor exports

---
+++ input.js
@@ -0,0 +1,25 @@
+const net = require("net");
+// need to use stdin
+const { stdin } = require("process");
+
+const handleUserInput = function () {
+  //terminates game
+  stdin.on("data", (key) => {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  });
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = {
+  setupInput,
+};
--- play.js
+++ play.js
@@ -1,24 +1,7 @@
 const net = require("net");
 const { stdin } = require("process");
 const { connect } = require("./client");
-
-const handleUserInput = function () {
-  //terminates game
-  stdin.on("data", (key) => {
-    if (key === "\u0003") {
-      process.exit();
-    }
-  });
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
